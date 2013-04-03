a085d1757d7e25909aa1e26b5d78cc3c66983a1b
Michael Tracy <mike.a.tracy@gmail.com>
2013-04-03 13:16:47 -0400

    Move calc's VM from code-behind to a real class.

---
+++ TestProject/CalcVM.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Util;
+
+namespace TestProject
+{
+    public class CalcVM : INotifyPropertyChanged
+    {
+        private string _output;
+        public string Output 
+        {
+            get { return _output; }
+            set
+            {
+                _output = value;
+                OnPropertyChanged("Output");
+            }
+        }
+
+        private RelayCommand _number;
+        public ICommand Number
+        {
+            get
+            {
+                return _number ?? (_number = new RelayCommand(n => Output += n));
+            }
+        }
+
+        private RelayCommand _do;
+        public ICommand Do
+        {
+            get
+            {
+                return _do ?? (_do = new RelayCommand(d => Calculate(d.ToString())));
+            }
+        }
+
+        private int result = 0;
+        public void Calculate(string action)
+        {
+            int output;
+            if (!int.TryParse(Output, out output))
+                return;
+            result += output;
+            switch (action)
+            {
+                case "=":
+                    Output = result.ToString();
+                    break;
+                case "+":
+                    Output = "";
+                    break;
+                case "-":
+                    Output = "-";
+                    break;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    
+    }
+}
