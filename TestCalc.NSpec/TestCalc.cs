e3e2ced06ca4ae260ad4e4b6ab38bbd9397015bf
Michael Tracy <mike.a.tracy@gmail.com>
2013-04-03 13:31:30 -0400

    Create some NSpec tests.

---
+++ TestCalc.NSpec/TestCalc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NSpec;
+using TestProject;
+
+namespace TestCalc.NSpec
+{
+    class TestCalc : nspec
+    {
+        CalcVM calc;
+
+        void before_each()
+        {
+            calc = new CalcVM();
+        }
+
+        void describe_pushing_buttons() {
+            context["when I push 1"] = () =>
+            {
+                before = () => calc.Number.Execute("1");
+                it["should display '1'"] = () => calc.Output.should_be("1");
+
+                context["then I push 2"] = () => {
+                    before = () => calc.Number.Execute("2");
+                    it["should display 12"] = () => calc.Output.should_be("12");
+
+                    context["then I push equals"] = () =>
+                    {
+                        before = () => calc.Do.Execute("=");
+                        it["should still show 12"] = () => calc.Output.should_be("12");
+                    };
+                };
+            };
+        }
+    }
+}
