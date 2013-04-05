5f243262206a990c948dfbfb0df2b9cc7c701bb6
Michael Tracy <mike.a.tracy@gmail.com>
2013-04-05 08:54:28 -0400

    Add a specflow spec for...adding

---
+++ TestCalc.Specs/Calculon.feature
@@ -0,0 +1,14 @@
+﻿Feature: Calculon
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Background: 
+	Given I started calculon
+
+Scenario: Add two numbers
+	Given I have entered 50 into the calculator
+	And I press "+"
+	And I have entered 70 into the calculator
+	When I press "="
+	Then the result should be 120 on the screen
---
+++ TestCalc.Specs/CalculonSteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using TestProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCalc.Specs
+{
+    [Binding]
+    public class CalculonSteps
+    {
+        CalcVM calc;
+
+        [Given(@"I started calculon")]
+        public void GivenIStartedCalculon()
+        {
+            calc = new CalcVM();
+        }
+        
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            calc.Number.Execute(p0);
+        }
+        
+        [Given(@"I press ""(.*)""")]
+        public void GivenIPress(string p0)
+        {
+            calc.Do.Execute(p0);
+        }
+        
+        [When(@"I press ""(.*)""")]
+        public void WhenIPress(string p0)
+        {
+            calc.Do.Execute(p0);
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(p0.ToString(), calc.Output);
+        }
+    }
+}
